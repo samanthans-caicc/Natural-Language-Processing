978cd9ece78640d6d9cebd88b2aa5a97e22d5cd6
samanthans-caicc <Samamtha.Salas@my.utsa.edu>
2026-02-12 10:59:21 -0600

    commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/sqmi/Natural-Language-Processing/Natural-Language-Processing/venv
+set -gx VIRTUAL_ENV '/home/sqmi/Natural Language Processing/Natural-Language-Processing/venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
